abe7454276b8477a065aacd226803320e32758fc
Mateus <mateusargento@yahoo.com.br>
2025-05-16 04:05:20 -0300

    adicionando transições no menu desktop
    adicionando as funcionalidades
    - link em negrito ao rolar para tópico correspondente a ele
    - box-shadow do menu a partir de uma determinada altura ao rolagem
    - transição no css para adicionar suavidade na troca de efeitos
    - adicionando novas constantes para que o código fique mais explicado

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Links do menu desktop
-    const linksDesktop = document.querySelectorAll('.menu-desktop-link')
-    const projectsButton = document.querySelector('#buttonProjects')
-
     function handleScroll(event) {
         event.preventDefault()
 
         const id = this.getAttribute('href') || this.getAttribute('form')
-        const idFormatted = id.substring(1)
+        const idFormatted = id.substring(1) // Remove o "#" do id
         const selectedSection = document.getElementById(idFormatted)
         const headerHeight = 100
 
@@ -22,10 +18,46 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     // Ao clicar nos links do menu
-    linksDesktop.forEach((link) => {
+    const linksMenuDesktop = document.querySelectorAll('.menu-desktop-link')
+    linksMenuDesktop.forEach((link) => {
         link.addEventListener('click', handleScroll)
     })
 
     // Ao clicar no botão para visualizar os projetos
+    const projectsButton = document.querySelector('#buttonProjects')
     projectsButton.addEventListener('click', handleScroll)
+
+
+    // Ao rolar a tela, ativa o box-shadow do menu desktop
+    // Ao rolar a tela, deixa em negrito o link do menu correspondente ao tópico que está sendo mostrado na tela
+    const sectionList = document.querySelectorAll('main > section')
+    let sectionOffsetTopList = []
+    sectionList.forEach((section) => sectionOffsetTopList.push(section.offsetTop))
+
+    window.addEventListener('scroll', function () {
+        const scrollY = window.scrollY
+        const minScrollY = 100
+        const screenWidth = window.screen.width
+        const minWidthDesktop = 1024
+        const boxShadow = "0 2px 4px #00000025"
+
+        scrollY >= minScrollY && screenWidth >= minWidthDesktop
+            ? document.getElementsByTagName('header')[0].style.boxShadow = boxShadow
+            : document.getElementsByTagName('header')[0].style.boxShadow = ""
+
+        let smallerSectionOffsetTopAndScrollYDifference = sectionOffsetTopList[sectionList.length - 1]
+
+        sectionOffsetTopList.forEach((sectionOffsetTop, i) => {
+            const sectionOffsetTopAndScrollYDifference = Math.abs(sectionOffsetTop - scrollY)
+
+            if (sectionOffsetTopAndScrollYDifference <= smallerSectionOffsetTopAndScrollYDifference) {
+                smallerSectionOffsetTopAndScrollYDifference = sectionOffsetTopAndScrollYDifference
+
+                linksMenuDesktop.forEach((item) => {
+                    item.style.fontWeight = 'normal'
+                })
+                linksMenuDesktop[i].style.fontWeight = 'bold'
+            }
+        })
+    })
 })
