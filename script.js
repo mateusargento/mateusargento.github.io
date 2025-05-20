ecaba66650ef95bc88105b1d53510b1b22874f8f
Mateus <mateusargento@yahoo.com.br>
2025-05-20 01:42:00 -0300

    adicionando menu mobile

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Controla o menu mobile
+    const menuMobileBg = document.querySelector('.menu-mobile-content-bg')
+    const openMenuButton = document.querySelector('#menu-mobile-icon-open')
+    const closeMenuButton = document.querySelector('#menu-mobile-icon-close')
+
+    function handleMenuMobile(action) {
+        const menuMobileContent = document.querySelector('.menu-mobile-content')
+
+        if (action == 'open') {
+            menuMobileBg.style.opacity = '1'
+            menuMobileBg.style.visibility = 'visible'
+            menuMobileContent.style.opacity = '1'
+            menuMobileContent.style.visibility = 'visible'
+            menuMobileContent.style.width = '70vw'
+        }
+
+        if (action == 'close') {
+            menuMobileBg.style.opacity = '0'
+            menuMobileBg.style.visibility = 'hidden'
+            menuMobileContent.style.opacity = '0'
+            menuMobileContent.style.visibility = 'hidden'
+            menuMobileContent.style.width = '0vw'
+        }
+    }
+
+    openMenuButton.addEventListener('click', () => handleMenuMobile('open'))
+    closeMenuButton.addEventListener('click', () => handleMenuMobile('close'))
+    menuMobileBg.addEventListener('click', () => handleMenuMobile('close'))
+
+
+
     function handleScroll(event) {
         event.preventDefault()
 
@@ -6,6 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const idFormatted = id.substring(1) // Remove o "#" do id
         const selectedSection = document.getElementById(idFormatted)
         const headerHeight = 100
+        const screenWidth = window.screen.width
 
         if (selectedSection) {
             const top = selectedSection.offsetTop - headerHeight
@@ -14,22 +46,30 @@ document.addEventListener('DOMContentLoaded', function () {
                 top: top,
                 behavior: 'smooth'
             })
+
+            screenWidth < 1024 && handleMenuMobile('close')
         }
     }
 
-    // Ao clicar nos links do menu
+    // Ao clicar nos links do menu mobile
+    const linksMenuMobile = document.querySelectorAll('.menu-mobile-link')
+    linksMenuMobile.forEach((link) => {
+        link.addEventListener('click', handleScroll)
+    })
+
+    // Ao clicar nos links do menu desktop
     const linksMenuDesktop = document.querySelectorAll('.menu-desktop-link')
     linksMenuDesktop.forEach((link) => {
         link.addEventListener('click', handleScroll)
     })
 
-    // Ao clicar no botão para visualizar os projetos
+    // Ao clicar no botão da página inicial para visualizar os projetos
     const projectsButton = document.querySelector('#buttonProjects')
     projectsButton.addEventListener('click', handleScroll)
 
 
-    // Ao rolar a tela, ativa o box-shadow do menu desktop
-    // Ao rolar a tela, deixa em negrito o link do menu correspondente ao tópico que está sendo mostrado na tela
+    // Ao rolar a tela, ativa o box-shadow do menu mobile e do menu desktop
+    // Ao rolar a tela, deixa em negrito o link do menu desktop correspondente ao tópico que está sendo mostrado na tela
     const sectionList = document.querySelectorAll('main > section')
     let sectionOffsetTopList = []
     sectionList.forEach((section) => sectionOffsetTopList.push(section.offsetTop))
@@ -41,23 +81,25 @@ document.addEventListener('DOMContentLoaded', function () {
         const minWidthDesktop = 1024
         const boxShadow = "0 2px 4px #00000025"
 
-        scrollY >= minScrollY && screenWidth >= minWidthDesktop
+        scrollY >= minScrollY
             ? document.getElementsByTagName('header')[0].style.boxShadow = boxShadow
             : document.getElementsByTagName('header')[0].style.boxShadow = ""
 
-        let smallerSectionOffsetTopAndScrollYDifference = sectionOffsetTopList[sectionList.length - 1]
+        if (screenWidth >= minWidthDesktop) {
+            let smallerSectionOffsetTopAndScrollYDifference = sectionOffsetTopList[sectionList.length - 1]
 
-        sectionOffsetTopList.forEach((sectionOffsetTop, i) => {
-            const sectionOffsetTopAndScrollYDifference = Math.abs(sectionOffsetTop - scrollY)
+            sectionOffsetTopList.forEach((sectionOffsetTop, i) => {
+                const sectionOffsetTopAndScrollYDifference = Math.abs(sectionOffsetTop - scrollY)
 
-            if (sectionOffsetTopAndScrollYDifference <= smallerSectionOffsetTopAndScrollYDifference) {
-                smallerSectionOffsetTopAndScrollYDifference = sectionOffsetTopAndScrollYDifference
+                if (sectionOffsetTopAndScrollYDifference <= smallerSectionOffsetTopAndScrollYDifference) {
+                    smallerSectionOffsetTopAndScrollYDifference = sectionOffsetTopAndScrollYDifference
 
-                linksMenuDesktop.forEach((item) => {
-                    item.style.fontWeight = 'normal'
-                })
-                linksMenuDesktop[i].style.fontWeight = 'bold'
-            }
-        })
+                    linksMenuDesktop.forEach((item) => {
+                        item.style.fontWeight = 'normal'
+                    })
+                    linksMenuDesktop[i].style.fontWeight = 'bold'
+                }
+            })
+        }
     })
 })
